5bcd1d956fee6a77e064c69d5c0bb3bf41cf64b8
Sheikh-369 <sheikhzalam@gmail.com>
2025-07-08 19:50:42 +0545

    instaFollow

--- Friend Request/index.js
+++ Friend Request/index.js
@@ -15,11 +15,3 @@ button.addEventListener("click",()=>{
     
 
 })
-
-echo "# Insta-Follow-Concept" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Sheikh-369/Insta-Follow-Concept.git
-git push -u origin main
